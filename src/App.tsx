be1d63a92a4d23dad132f5bc0ad28f6586e2cf81
verseler <handuman.verselerkerr@gmail.com>
2024-08-22 20:27:05 +0800

    feat: handling user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 
+import { Toaster } from "./components/ui/toaster";
 import NotFound from "@/pages/NotFound";
 import Login from "@/pages/unauthenticated/Login";
 import Register from "@/pages/unauthenticated/Register";
+import AuthRoute from "./components/auth/AuthRoute";
+import Home from "./pages/authenticated/home";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Register />} />
-      <Route path="login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="bg-white dark:bg-gray-900">
+      <Routes>
+        <Route element={<AuthRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Toaster />
+    </div>
   );
 };
 
